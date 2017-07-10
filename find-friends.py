5981ecc90ad7b6ac836027b307fa9377c99deaaa
Okonechnikov Vladimir <Okonechnikov.Vladimir@infotecs.ru>
2017-07-10 23:32:17 +0300

    Add find friends.

--- find-friends.py
+++ find-friends.py
@@ -1,20 +1,14 @@
 class Tree:
     def __init__(self, name, links=None):
         self.name = name
-        self.links = links if links else []
-
-    def __str__(self):
-        return str(self.name)
+        self.links = links if links else ()
 
 
 def populate_tree(name: str, network: tuple):
-    found_links = tuple(filter(lambda x: name in x, network))
-    other_links = tuple(x for x in network if x not in found_links)
+    found_links = (n.replace(name, '').replace('-', '') for n in network if name in n)
+    other_links = tuple(n for n in network if name not in n)
 
-    links = []
-    for link in found_links:
-        node = link.replace(name, '').replace('-', '')
-        links.append(populate_tree(node, other_links))
+    links = (populate_tree(l, other_links) for l in found_links)
     return Tree(name, links)
 
 
@@ -29,19 +23,3 @@ def search_tree(tree: Tree, find_name):
 def check_connection(network, first, second):
     tree = populate_tree(first, network)
     return search_tree(tree, second)
-
-
-if __name__ == '__main__':
-    # These "asserts" using only for self-checking and not necessary for auto-testing
-    assert check_connection(
-        ("dr101-mr99", "mr99-out00", "dr101-out00", "scout1-scout2",
-         "scout3-scout1", "scout1-scout4", "scout4-sscout", "sscout-super"),
-        "scout2", "scout3") == True, "Scout Brotherhood"
-    assert check_connection(
-        ("dr101-mr99", "mr99-out00", "dr101-out00", "scout1-scout2",
-         "scout3-scout1", "scout1-scout4", "scout4-sscout", "sscout-super"),
-        "super", "scout2") == True, "Super Scout"
-    assert check_connection(
-        ("dr101-mr99", "mr99-out00", "dr101-out00", "scout1-scout2",
-         "scout3-scout1", "scout1-scout4", "scout4-sscout", "sscout-super"),
-        "dr101", "sscout") == False, "I don't know any scouts."
