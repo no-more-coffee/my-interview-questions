2e6e842f522bb6694e3a8e65578ccd83c1dc27ee
Okonechnikov Vladimir <Okonechnikov.Vladimir@infotecs.ru>
2017-07-12 00:18:55 +0300

    Add second find-friends.

--- find-friends1.py
+++ find-friends1.py
@@ -1,28 +1,12 @@
-class Tree:
-    def __init__(self, name, links=None):
-        self.name = name
-        self.links = links if links else ()
-
-
-def populate_tree(name: str, network: tuple):
-    other_links = tuple(n for n in network if name not in n)
-    found_links = (n.replace(name, '').replace('-', '') for n in network if name in n)
-
-    links = (populate_tree(l, other_links) for l in found_links)
-    return Tree(name, links)
-
-
-def search_tree(tree: Tree, find_name):
-    if tree.name == find_name:
-        return True
-    for node in tree.links:
-        return search_tree(node, find_name)
-    return False
-
-
 def check_connection(network, first, second):
-    tree = populate_tree(first, network)
-    return search_tree(tree, second)
+    connections = {first}
+    for _ in network:
+        for connected in network:
+            if connections & set(connected.split("-")):
+                if second in connections:
+                    return True
+                connections |= set(connected.split("-"))
+    return False
 
 
 if __name__ == '__main__':
