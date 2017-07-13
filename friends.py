0cb399289d35aae54bca5454ab4782891d503b31
Okonechnikov Vladimir <Okonechnikov.Vladimir@infotecs.ru>
2017-07-13 23:18:29 +0300

    Add friends.

--- friends.py
+++ friends.py
@@ -1,28 +1,25 @@
+from itertools import chain
+
+
 class Friends:
     def __init__(self, connections):
-        raise NotImplementedError
+        self.connections = set(frozenset(x) for x in connections)
 
     def add(self, connection):
-        raise NotImplementedError
+        if connection in self.connections:
+            return False
+        self.connections.add(frozenset(connection))
+        return True
 
     def remove(self, connection):
-        raise NotImplementedError
+        try:
+            self.connections.remove(connection)
+            return True
+        except KeyError:
+            return False
 
     def names(self):
-        raise NotImplementedError
+        return set(chain.from_iterable(self.connections))
 
     def connected(self, name):
-        raise NotImplementedError
-
-
-if __name__ == '__main__':
-    # These "asserts" using only for self-checking and not necessary for auto-testing
-    letter_friends = Friends(({"a", "b"}, {"b", "c"}, {"c", "a"}, {"a", "c"}))
-    digit_friends = Friends([{"1", "2"}, {"3", "1"}])
-    assert letter_friends.add({"c", "d"}) is True, "Add"
-    assert letter_friends.add({"c", "d"}) is False, "Add again"
-    assert letter_friends.remove({"c", "d"}) is True, "Remove"
-    assert digit_friends.remove({"c", "d"}) is False, "Remove non exists"
-    assert letter_friends.names() == {"a", "b", "c"}, "Names"
-    assert letter_friends.connected("d") == set(), "Non connected name"
-    assert letter_friends.connected("a") == {"b", "c"}, "Connected name"
+        return set(chain.from_iterable((c - {name}) for c in self.connections if name in c))
