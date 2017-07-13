c1e06cf3f8d250fe0b298cf414bc84009bf29daf
Okonechnikov Vladimir <Okonechnikov.Vladimir@infotecs.ru>
2017-07-13 22:41:47 +0300

    Add feed pigeons.

--- feed-pigeons.py
+++ feed-pigeons.py
@@ -1,9 +1,8 @@
 def checkio(number):
-    return 0
-
-if __name__ == '__main__':
-    #These "asserts" using only for self-checking and not necessary for auto-testing
-    assert checkio(1) == 1, "1st example"
-    assert checkio(2) == 1, "2nd example"
-    assert checkio(5) == 3, "3rd example"
-    assert checkio(10) == 6, "4th example"
+    birds, fed_birds, minutes = 1, 1, 1
+    while number > birds:
+        number -= birds
+        fed_birds = birds
+        minutes += 1
+        birds += minutes
+    return max((fed_birds, number))
