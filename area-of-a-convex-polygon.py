2b5148c493b594eaec9024980ff0939306c67960
Okonechnikov Vladimir <Okonechnikov.Vladimir@infotecs.ru>
2017-08-09 00:41:34 +0300

    ! Add area of convex.

---
+++ area-of-a-convex-polygon.py
@@ -0,0 +1,27 @@
+def solve_triangle(data):
+    (x1, y1), (x2, y2), (x3, y3) = data
+    return abs((x1 - x3) * (y2 - y3) - (x2 - x3) * (y1 - y3)) / 2
+
+
+def checkio(data):
+    l = len(data)
+    d = [[data[(i + 2 * j) % l] for i in range(3)] for j in range(l - 2)]
+    print(d)
+    f = sum(solve_triangle(i) for i in d)
+    print(f)
+    return f
+
+
+if __name__ == '__main__':
+    # This part is using only for self-checking and not necessary for auto-testing
+    def almost_equal(checked, correct, significant_digits=1):
+        precision = 0.1 ** significant_digits
+        return correct - precision < checked < correct + precision
+
+
+    assert almost_equal(checkio([[1, 1], [9, 9], [9, 1]]), 32), "The half of the square"
+    assert almost_equal(checkio([[4, 10], [7, 1], [1, 4]]), 22.5), "Triangle"
+    assert almost_equal(checkio([[1, 2], [3, 8], [9, 8], [7, 1]]), 40), "Quadrilateral"
+    assert almost_equal(checkio([[3, 3], [2, 7], [5, 9], [8, 7], [7, 3]]), 26), "Pentagon"
+    assert almost_equal(checkio([[7, 2], [3, 2], [1, 5], [3, 9], [7, 9], [9, 6]]), 42), "Hexagon"
+    assert almost_equal(checkio([[4, 1], [3, 4], [3, 7], [4, 8], [7, 9], [9, 6], [7, 1]]), 35.5), "Heptagon"
---
+++ rotate-hole.py
@@ -0,0 +1,8 @@
+rotate = lambda s, p: [i for i in range(len(s)) if all(s[x - i] for x in p)]
+
+if __name__ == '__main__':
+    # These "asserts" using only for self-checking and not necessary for auto-testing
+    assert rotate([1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1], [0, 1]) == [1, 8], "Example"
+    assert rotate([1, 0, 0, 0, 1, 1, 0, 1, 0, 0, 0, 1], [0, 1, 2]) == [], "Mission impossible"
+    assert rotate([1, 0, 0, 0, 1, 1, 0, 1], [0, 4, 5]) == [0], "Don't touch it"
+    assert rotate([1, 0, 0, 0, 1, 1, 0, 1], [5, 4, 5]) == [0, 5], "Two cannonballs in the same pipe"
