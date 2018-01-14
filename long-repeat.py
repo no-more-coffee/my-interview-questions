0ff430bf52132efd52224a5c6e2932e64cbeb2dc
Okonechnikov Vladimir <Okonechnikov.Vladimir@infotecs.ru>
2018-01-14 16:57:12 +0300

    Add updated long repeat.

--- long-repeat.py
+++ long-repeat.py
@@ -1,24 +1,13 @@
+from itertools import groupby
+
+
 def long_repeat(line):
-    """
-        length the longest substring that consists of the same char
-    """
-    last_char = ''
-    count = 0
-    maximum = 0
-    for c in line:
-        if c != last_char:
-            maximum = max(count+1, maximum)
-            count = 0
-        else:
-            count += 1
-        last_char = c
-    max1 = max(count+1, maximum)
-    print(max1)
-    return max1
+    return max(len(list(v)) for k, v in groupby(line)) if line else 0
 
 
 if __name__ == '__main__':
     # These "asserts" using only for self-checking and not necessary for auto-testing
     assert long_repeat('sdsffffse') == 4, "First"
     assert long_repeat('ddvvrwwwrggg') == 3, "Second"
+    assert long_repeat("") == 0, "Zero"
     print('"Run" is good. How is "Check"?')
