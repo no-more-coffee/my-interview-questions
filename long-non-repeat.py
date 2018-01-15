4950146f9ca66a590dde58cd9d629ae01bc0193e
Okonechnikov Vladimir <Okonechnikov.Vladimir@infotecs.ru>
2018-01-15 22:01:05 +0300

    Add solution for long non repeat.

--- long-non-repeat.py
+++ long-non-repeat.py
@@ -2,12 +2,18 @@ def non_repeat(line):
     """
         the longest substring without repeating chars
     """
-    # your code here
-    return line
+    for i in range(len(line), 0, -1):
+        for j in range(0, len(line) - i + 1):
+            fragment = line[j: j + i]
+            if len(fragment) == len(set(fragment)):
+                return fragment
+    return ''
+
 
 if __name__ == '__main__':
-    #These "asserts" using only for self-checking and not necessary for auto-testing
+    # These "asserts" using only for self-checking and not necessary for auto-testing
     assert non_repeat('aaaaa') == 'a', "First"
     assert non_repeat('abdjwawk') == 'abdjw', "Second"
     assert non_repeat('abcabcffab') == 'abcf', "Third"
+    assert non_repeat("fghfrtyfgh") == 'ghfrty', "ghfrty"
     print('"Run" is good. How is "Check"?')
