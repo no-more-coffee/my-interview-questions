bc30abae254b431cff0d3e7565e9809d0fe05277
Okonechnikov Vladimir <Okonechnikov.Vladimir@infotecs.ru>
2020-02-10 08:52:30 +0300

    Add Flatten list.

---
+++ flatten-list.py
@@ -0,0 +1,18 @@
+def flat_list(array):
+    return list(flat(array))
+
+
+def flat(array):
+    for i in array:
+        if isinstance(i, list):
+            yield from flat(i)
+        else:
+            yield i
+
+
+if __name__ == '__main__':
+    assert flat_list([1, 2, 3]) == [1, 2, 3], "First"
+    assert flat_list([1, [2, 2, 2], 4]) == [1, 2, 2, 2, 4], "Second"
+    assert flat_list([[[2]], [4, [5, 6, [6], 6, 6, 6], 7]]) == [2, 4, 5, 6, 6, 6, 6, 6, 7], "Third"
+    assert flat_list([-1, [1, [-2], 1], -1]) == [-1, 1, -2, 1, -1], "Four"
+    print('Done! Check it')
