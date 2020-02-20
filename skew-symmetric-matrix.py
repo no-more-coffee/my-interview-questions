291b8ee0e09b90fec9bce1424901bebf465b49b5
Okonechnikov Vladimir <Okonechnikov.Vladimir@infotecs.ru>
2020-02-20 20:44:35 +0300

    Add skew-symmetric-matrix.

---
+++ skew-symmetric-matrix.py
@@ -0,0 +1,30 @@
+import numpy as np
+
+
+def checkio(matrix):
+    a = np.array(matrix)
+    at = a.transpose() * -1
+    return a == at
+
+
+# These "asserts" using only for self-checking and not necessary for auto-testing
+if __name__ == '__main__':
+    print("Example:")
+    print(checkio([
+        [0, 1, 2],
+        [-1, 0, 1],
+        [-2, -1, 0]]))
+
+    assert checkio([
+        [0, 1, 2],
+        [-1, 0, 1],
+        [-2, -1, 0]]) == True, "1st example"
+    assert checkio([
+        [0, 1, 2],
+        [-1, 1, 1],
+        [-2, -1, 0]]) == False, "2nd example"
+    assert checkio([
+        [0, 1, 2],
+        [-1, 0, 1],
+        [-3, -1, 0]]) == False, "3rd example"
+    print("Coding complete? Click 'Check' to earn cool rewards!");
