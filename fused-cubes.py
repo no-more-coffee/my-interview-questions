6248294e53a7fb40d85e742a0316072185ce44a7
Okonechnikov Vladimir <Okonechnikov.Vladimir@infotecs.ru>
2020-03-15 22:10:57 +0500

    Add failing fused-cubes.1

--- fused-cubes.py
+++ fused-cubes.py
@@ -1,17 +1,33 @@
 from typing import Tuple, List, Iterable
 
 
-def intersection(x1, x2, l1, l2):
+def intersection(x1, x2, l1, l2) -> int:
     return min(x1 + l1, x2 + l2) - max(x1, x2)
 
 
+def get_intersections(c1: Tuple[int, int, int, int], c2: Tuple[int, int, int, int]):
+    return [intersection(c1[i], c2[i], c1[3], c2[3]) for i in range(3)]
+
+
+def has_intersects(intersections):
+    return len(tuple(a for a in intersections if a < 0)) == 0 and len(tuple(a for a in intersections if a == 0)) <= 1
+
+
 def fused_cubes(cubes: List[Tuple[int, int, int, int]]) -> Iterable[int]:
-    a1 = intersection(cubes[0][0], cubes[1][0], cubes[0][3], cubes[1][3])
-    a2 = intersection(cubes[0][1], cubes[1][1], cubes[0][3], cubes[1][3])
-    a3 = intersection(cubes[0][2], cubes[1][2], cubes[0][3], cubes[1][3])
-    if len(tuple(a for a in (a1, a2, a3) if a < 0)) or len(tuple(a for a in (a1, a2, a3) if a == 0)) > 1:
-        return [cubes[1][3] ** 3, cubes[0][3] ** 3]
-    return [cubes[1][3] ** 3 + cubes[0][3] ** 3 - (a1 * a2 * a3)]
+    groups = []
+    for c in cubes:
+        new_group = {c}
+        new_groups = [new_group]
+        for g in groups:
+            if any(has_intersects(get_intersections(c, c1)) for c1 in g):
+                new_group |= g
+            else:
+                new_groups.append(g)
+        groups = new_groups
+
+    # if not has_intersects(get_intersections()):
+    #     return [cubes[1][3] ** 3, cubes[0][3] ** 3]
+    # return [cubes[1][3] ** 3 + cubes[0][3] ** 3 - (a1 * a2 * a3)]
 
 
 if __name__ == '__main__':
