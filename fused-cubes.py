495af860efb3e6d9757366777ad450466724c9eb
Okonechnikov Vladimir <Okonechnikov.Vladimir@infotecs.ru>
2020-03-16 21:49:22 +0500

    Add failing fused-cubes.2

--- fused-cubes.py
+++ fused-cubes.py
@@ -1,3 +1,6 @@
+from functools import reduce
+from itertools import combinations
+from operator import mul
 from typing import Tuple, List, Iterable
 
 
@@ -16,7 +19,7 @@ def has_intersects(intersections):
 def fused_cubes(cubes: List[Tuple[int, int, int, int]]) -> Iterable[int]:
     groups = []
     for c in cubes:
-        new_group = {c}
+        new_group = {tuple(c)}
         new_groups = [new_group]
         for g in groups:
             if any(has_intersects(get_intersections(c, c1)) for c1 in g):
@@ -25,9 +28,17 @@ def fused_cubes(cubes: List[Tuple[int, int, int, int]]) -> Iterable[int]:
                 new_groups.append(g)
         groups = new_groups
 
-    # if not has_intersects(get_intersections()):
-    #     return [cubes[1][3] ** 3, cubes[0][3] ** 3]
-    # return [cubes[1][3] ** 3 + cubes[0][3] ** 3 - (a1 * a2 * a3)]
+    for g in groups:
+        yield get_area(g)
+
+
+def get_area(group):
+    res = sum(c[3] ** 3 for c in group)
+    for c1, c2 in combinations(group, 2):
+        intersections = get_intersections(c1, c2)
+        if has_intersects(intersections):
+            res -= reduce(mul, intersections)
+    return res
 
 
 if __name__ == '__main__':
@@ -40,4 +51,13 @@ if __name__ == '__main__':
     assert sorted(
         fused_cubes([[-1, 0, 0, 1], [1, 0, 0, 1], [0, 1, 0, 1], [0, -1, 0, 1], [0, 0, 1, 1], [0, 0, -1, 1]])
     ) == [1, 1, 1, 1, 1, 1], 'many'
+    l1 = sorted(fused_cubes(
+        [[0, 0, -10, 1], [0, 0, -9, 1], [0, 0, -8, 1], [0, 0, -7, 1], [0, 0, -6, 1], [3, 0, -10, 1], [3, 0, -9, 1],
+         [3, 0, -8, 1], [3, 0, -7, 1], [3, 0, -6, 1], [1, 4, -6, 2], [0, 0, -5, 2], [2, 0, -5, 2], [0, 1, -5, 2],
+         [2, 1, -5, 2], [1, 3, -5, 2], [2, 0, -3, 2], [1, 2, -3, 2], [1, 1, -1, 2], [1, 0, 0, 2], [1, 0, 2, 2],
+         [3, 0, 1, 1], [3, 0, 2, 1], [3, 0, 3, 1], [-9, 2, 6, 1], [-10, 0, 5, 2], [-9, 0, 5, 2], [-9, 0, 6, 2],
+         [-11, 0, 4, 1], [-10, 0, 4, 1], [-9, 0, 4, 1], [-8, 0, 4, 1], [-7, 0, 4, 1], [-7, 0, 5, 1], [-7, 0, 6, 1],
+         [-7, 0, 7, 1], [-7, 0, 8, 1], [-8, 0, 8, 1], [-11, 0, 5, 1]]))
+    assert l1 == [28, 89], f'Should be [28,89], got {l1}'
+
     print("Coding complete? Click 'Check' to earn cool rewards!")
