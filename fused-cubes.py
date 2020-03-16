7db38ccd2c3b1dc5ee0a339958166aec37c5f35c
Okonechnikov Vladimir <Okonechnikov.Vladimir@infotecs.ru>
2020-03-16 22:15:46 +0500

    Add fused-cubes.

--- fused-cubes.py
+++ fused-cubes.py
@@ -1,6 +1,3 @@
-from functools import reduce
-from itertools import combinations
-from operator import mul
 from typing import Tuple, List, Iterable
 
 
@@ -14,6 +11,14 @@ def get_intersections(c1: Tuple[int, int, int, int], c2: Tuple[int, int, int, in
 
 def has_intersects(intersections):
     return len(tuple(a for a in intersections if a < 0)) == 0 and len(tuple(a for a in intersections if a == 0)) <= 1
+
+
+def get_dots(cube: Tuple[int, int, int, int]):
+    x, y, z, l = cube
+    for x1 in range(x, x + l):
+        for y1 in range(y, y + l):
+            for z1 in range(z, z + l):
+                yield x1, y1, z1
 
 
 def fused_cubes(cubes: List[Tuple[int, int, int, int]]) -> Iterable[int]:
@@ -32,32 +37,39 @@ def fused_cubes(cubes: List[Tuple[int, int, int, int]]) -> Iterable[int]:
         yield get_area(g)
 
 
+def get_group_dots(group):
+    for c in group:
+        yield from get_dots(c)
+
+
 def get_area(group):
-    res = sum(c[3] ** 3 for c in group)
-    for c1, c2 in combinations(group, 2):
-        intersections = get_intersections(c1, c2)
-        if has_intersects(intersections):
-            res -= reduce(mul, intersections)
-    return res
+    return len(set(get_group_dots(group)))
+
+
+def assert1(r, r1, message):
+    lasdf = sorted(fused_cubes(r))
+    assert lasdf == r1, f'{message} {lasdf}!={r1}'
 
 
 if __name__ == '__main__':
-    assert sorted(fused_cubes([(0, 0, 0, 3), (1, 2, 2, 3)])) == [52], 'fused'
-    assert sorted(fused_cubes([(0, 0, 0, 3), (1, 3, 2, 3)])) == [54], 'touch with faces'
-    assert sorted(fused_cubes([(0, 0, 0, 3), (1, 3, 3, 3)])) == [27, 27], 'touch with edges'
-    assert sorted(fused_cubes([(0, 0, 0, 3), (3, 3, 3, 3)])) == [27, 27], 'touch with vertices'
-    assert sorted(fused_cubes([(0, 0, 0, 3), (3, 4, 3, 3)])) == [27, 27], 'separated'
-    assert sorted(fused_cubes([(0, 0, 0, 3), (-2, -2, -2, 3)])) == [53], 'negative coordinates'
-    assert sorted(
-        fused_cubes([[-1, 0, 0, 1], [1, 0, 0, 1], [0, 1, 0, 1], [0, -1, 0, 1], [0, 0, 1, 1], [0, 0, -1, 1]])
-    ) == [1, 1, 1, 1, 1, 1], 'many'
-    l1 = sorted(fused_cubes(
+    assert1([(0, 0, 0, 3), (1, 2, 2, 3)], [52], 'fused')
+    assert1([(0, 0, 0, 3), (1, 3, 2, 3)], [54], 'touch with faces')
+    assert1([(0, 0, 0, 3), (1, 3, 3, 3)], [27, 27], 'touch with edges')
+    assert1([(0, 0, 0, 3), (3, 3, 3, 3)], [27, 27], 'touch with vertices')
+    assert1([(0, 0, 0, 3), (3, 4, 3, 3)], [27, 27], 'separated')
+    assert1([(0, 0, 0, 3), (-2, -2, -2, 3)], [53], 'negative coordinates')
+    assert1(
+        [[-1, 0, 0, 1], [1, 0, 0, 1], [0, 1, 0, 1], [0, -1, 0, 1], [0, 0, 1, 1], [0, 0, -1, 1]],
+        [1, 1, 1, 1, 1, 1],
+        'many')
+    assert1(
         [[0, 0, -10, 1], [0, 0, -9, 1], [0, 0, -8, 1], [0, 0, -7, 1], [0, 0, -6, 1], [3, 0, -10, 1], [3, 0, -9, 1],
          [3, 0, -8, 1], [3, 0, -7, 1], [3, 0, -6, 1], [1, 4, -6, 2], [0, 0, -5, 2], [2, 0, -5, 2], [0, 1, -5, 2],
          [2, 1, -5, 2], [1, 3, -5, 2], [2, 0, -3, 2], [1, 2, -3, 2], [1, 1, -1, 2], [1, 0, 0, 2], [1, 0, 2, 2],
          [3, 0, 1, 1], [3, 0, 2, 1], [3, 0, 3, 1], [-9, 2, 6, 1], [-10, 0, 5, 2], [-9, 0, 5, 2], [-9, 0, 6, 2],
          [-11, 0, 4, 1], [-10, 0, 4, 1], [-9, 0, 4, 1], [-8, 0, 4, 1], [-7, 0, 4, 1], [-7, 0, 5, 1], [-7, 0, 6, 1],
-         [-7, 0, 7, 1], [-7, 0, 8, 1], [-8, 0, 8, 1], [-11, 0, 5, 1]]))
-    assert l1 == [28, 89], f'Should be [28,89], got {l1}'
+         [-7, 0, 7, 1], [-7, 0, 8, 1], [-8, 0, 8, 1], [-11, 0, 5, 1]],
+        [28, 89],
+        '')
 
     print("Coding complete? Click 'Check' to earn cool rewards!")
