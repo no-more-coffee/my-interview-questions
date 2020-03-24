2e242bd911bc069be4c33c2c0fc674e8b6808ee8
Okonechnikov Vladimir <Okonechnikov.Vladimir@infotecs.ru>
2020-03-24 14:54:30 +0500

    Add Symmetric Tree iterative.

--- leet/Weekly Contest 181/Symmetric Tree.py
+++ leet/Weekly Contest 181/Symmetric Tree.py
@@ -24,3 +24,23 @@ def do(*nodes):
         return False
 
     return do(*children)
+
+
+class SolutionIterative:
+    def isSymmetric(self, root: TreeNode) -> bool:
+        nodes = [root]
+        while nodes:
+            children = []
+            for n in nodes:
+                if not n:
+                    continue
+                children.append(n.left)
+                children.append(n.right)
+            if not children:
+                return True
+            for i in range(len(children) // 2 + 1):
+                left_child = children[i]
+                right_child = children[-i-1]
+                if (left_child and left_child.val) != (right_child and right_child.val):
+                    return False
+            nodes = children
