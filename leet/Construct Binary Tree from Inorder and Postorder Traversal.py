c2cb417e2c8361603de34393d70dc668741e5ca0
Okonechnikov Vladimir <Okonechnikov.Vladimir@infotecs.ru>
2020-03-29 19:04:26 +0500

    Add Construct Binary Tree from Inorder and Postorder Traversal from geeksforgeeks.

--- leet/Construct Binary Tree from Inorder and Postorder Traversal.py
+++ leet/Construct Binary Tree from Inorder and Postorder Traversal.py
@@ -12,34 +12,29 @@ class TreeNode:
 
 
 class Solution:
+    def __init__(self) -> None:
+        self.post_i = 0
+
     def buildTree(self, inorder: List[int], postorder: List[int]) -> TreeNode:
-        def do(inorder, post_i):
-            if not inorder:
+        def do(in_first, in_last):
+            if in_first > in_last:
                 return None
 
-            while postorder[post_i] not in inorder:
-                post_i -= 1
+            current = postorder[self.post_i]
+            node = TreeNode(current)
+            self.post_i -= 1
 
-            node = TreeNode(postorder[post_i])
+            if in_first == in_last:
+                return node
 
-            is_left = True
-            left_in = []
-            right_in = []
-            for i in inorder:
-                if i == node.val:
-                    is_left = False
-                    continue
-
-                if is_left:
-                    left_in.append(i)
-                else:
-                    right_in.append(i)
-
-            node.right = do(right_in, post_i - 1)
-            node.left = do(left_in, post_i - 1)
+            current_i = in_indexes[current]
+            node.right = do(current_i + 1, in_last)
+            node.left = do(in_first, current_i - 1)
             return node
 
-        return do(inorder, -1)
+        self.post_i = len(postorder) - 1
+        in_indexes = {v: i for i, v in enumerate(inorder)}
+        return do(0, len(inorder) - 1)
 
 
 print(Solution().buildTree([9, 3, 15, 20, 7], [9, 15, 7, 20, 3]))
