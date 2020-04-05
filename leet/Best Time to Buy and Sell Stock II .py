7bfefde092463ec94e00088271e4328a452a9f61
Okonechnikov Vladimir <Okonechnikov.Vladimir@infotecs.ru>
2020-04-05 17:05:22 +0500

    Add Best Time to Buy and Sell Stock

--- leet/Best Time to Buy and Sell Stock II .py
+++ leet/Best Time to Buy and Sell Stock II .py
@@ -1,29 +1,25 @@
-from functools import lru_cache
 from typing import List
-
-
-@lru_cache
-def max_p(*prices):
-    result = 0
-    first_small = 0
-    for i in range(1, len(prices)):
-        if prices[i] <= prices[first_small]:
-            first_small += 1
-            continue
-        m = max(
-            (prices[l] - prices[first_small] + max_p(*prices[l + 1:]))
-            for l in range(i, len(prices))
-            if prices[l] > prices[first_small]
-        )
-        result = max(m, result)
-    return result
 
 
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        return max_p(*prices)
+        groups = []
+        current_group_min_i = 0
+        current_group_max_i = 0
+        for i in range(1, len(prices)):
+            if prices[i] > prices[i - 1]:
+                current_group_max_i = i
+            elif prices[i] < prices[i - 1]:
+                groups.append((current_group_min_i, current_group_max_i))
+                current_group_min_i = i
+                current_group_max_i = i
+                continue
+            if i == len(prices) - 1:
+                groups.append((current_group_min_i, current_group_max_i))
+        return sum(prices[y] - prices[x] for x, y in groups)
 
 
-print(Solution().maxProfit([7, 1, 5, 3, 6, 4]))
-print(Solution().maxProfit([1, 2, 3, 4, 5]))
-print(Solution().maxProfit([7, 6, 4, 3, 1]))
+print(Solution().maxProfit([7, 1, 5, 3, 6, 4]), 7)
+print(Solution().maxProfit([1, 2, 3, 4, 5]), 4)
+print(Solution().maxProfit([7, 6, 4, 3, 1]), 0)
+print(Solution().maxProfit([1, 9, 6, 9, 1, 7, 1, 1, 5, 9, 9, 9]), 25)
