22339ff3e7a050d3daf0ef7406339fa3572d11d7
Okonechnikov Vladimir <Okonechnikov.Vladimir@infotecs.ru>
2020-04-22 10:31:07 +0300

    Add working Product of Array Except Self.

--- leet/Product of Array Except Self.py
+++ leet/Product of Array Except Self.py
@@ -1,13 +1,25 @@
-from functools import reduce
 from typing import List
 
 
 class Solution:
     def productExceptSelf(self, nums: List[int]) -> List[int]:
-        all_product = reduce(lambda x, y: x * y, (i for i in nums if i), 1) if any(nums) else 0
-        return [round(all_product / i) if i else all_product for i in nums]
+        def build_L():
+            l = 1
+            yield l
+            for i in range(1, len(nums)):
+                l *= nums[i - 1]
+                yield l
+
+        res = list(build_L())
+
+        r = 1
+        for i in range(len(nums) - 1, -1, -1):
+            res[i] *= r
+            r *= nums[i]
+        return res
 
 
+print(Solution().productExceptSelf([4, 5, 1, 8, 2]), [80, 16, 16, 2, 1])
+print(Solution().productExceptSelf([1, 2, 3, 4]), [24, 12, 8, 6])
 print(Solution().productExceptSelf([1, 0]), [0, 1])
 print(Solution().productExceptSelf([0, 0]), [0, 0])
-print(Solution().productExceptSelf([1, 2, 3, 4]), [24, 12, 8, 6])
