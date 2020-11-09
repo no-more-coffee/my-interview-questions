76c232a6ecef0875cc309c899004b110d495de07
Okonechnikov Vladimir <Okonechnikov.Vladimir@infotecs.ru>
2020-11-09 22:46:34 +0300

    Add Maximum Subarray.2

--- leet/Maximum Subarray.py
+++ leet/Maximum Subarray.py
@@ -11,7 +11,7 @@ class Solution:
         return top
 
 
-class Solution:
+class Solution1:
     def maxSubArray(self, nums: List[int]) -> int:
         it = iter(nums)
         last_max = top = next(it)
@@ -19,3 +19,16 @@ class Solution:
             last_max = max(last_max + n, n)
             top = max(top, last_max)
         return top
+
+
+class Solution2:
+    def maxSubArray(self, nums: List[int]) -> int:
+        curr = 0
+        summ = max(nums)
+        for n in nums:
+            if curr + n > 0:
+                curr = curr + n
+                summ = max(summ, curr)
+            else:
+                curr = 0
+        return summ
